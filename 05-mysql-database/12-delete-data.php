e21936bedeb88daf30667d1d88737a80c6ee4003
zuoyi <654056708@qq.com>
2023-10-19 10:35:10 +0800

    delete data

--- 05-mysql-database/12-delete-data.php
+++ 05-mysql-database/12-delete-data.php
@@ -9,7 +9,8 @@
   try {
     $con = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $con->exec($sql);
+    $stmt = $con->prepare($sql);
+    $stmt->execute();
     echo 'Record deleted successfully.';
   } catch (PDOException $e) {
     echo $sql . "<br/>" . $e->getMessage();
