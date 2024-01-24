463584d0f79e1c61010ae2d7a08fc679d32de4ee
GavinZ5160 <gavin.z.5160@gmail.com>
2024-01-25 00:33:03 +0800

    Eloquent Builder and Query Builder, BaseCollection and Eloquent Collection

---
+++ learn-php-the-right-way/eloquents/eloquent_query6.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../eloquent.php';
+
+use App\Enums\InvoiceStatus;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+try {
+    Capsule::connection()->transaction(function () {
+        $invoices = Capsule::connection()
+            ->query()
+            ->from('invoices')
+            ->where('id', '=', 1)
+            ->get();
+        var_dump($invoices); // std class objects, which is from Base Query Builder, another is Eloquent Query Builder
+
+        $invoices = Capsule::connection()
+            ->table('invoices')
+            ->where('status', '=', InvoiceStatus::Paid);
+        // std class objects, which is from Base Query Builder, another is Eloquent Builder which has Query Builder as dependency
+        // same goes with Collections, Laravel has to Collections, one works with the eloquent, another one is base Collection
+        // var_dump($invoices);
+    });
+} catch (Throwable $e) {
+    var_dump($e->getMessage());
+}
