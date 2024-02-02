585eb9289b6bb8adb0257c44da4b1432cc57419f
GavinZ5160 <gavin.z.5160@gmail.com>
2024-02-02 21:39:02 +0800

    use twig in Slim

--- learn-php-the-right-way/index.php
+++ learn-php-the-right-way/index.php
@@ -2,38 +2,36 @@
 
 declare(strict_types=1);
 
-use App\{App, Router};
-use App\Controllers\{CurlController,
-    HomeController,
-    InvoiceController,
-    UserController
-};
-use Illuminate\Container\Container;
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+use Twig\Error\LoaderError;
+use Twig\Extra\Intl\IntlExtension;
 
 require_once 'vendor/autoload.php';
 
 const STORAGE_PATH = __DIR__.'/storage';
 const VIEW_PATH    = __DIR__.DIRECTORY_SEPARATOR.'views';
 
-$container = new Container();
-$router    = new Router($container);
-$request   = [
-    'uri'    => $_SERVER['REQUEST_URI'],
-    'method' => $_SERVER['REQUEST_METHOD'],
-];
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response) {
+    $view = Twig::fromRequest($request);
+
+    return $view->render($response, 'index.twig');
+});
 
 try {
-    $router->registerRoutesFromControllerAttributes(
-        [
-            HomeController::class,
-            UserController::class,
-            InvoiceController::class,
-            CurlController::class,
-        ]
-    );
-} catch (ReflectionException $e) {
+    $twig = Twig::create(VIEW_PATH, [
+        'cache'       => STORAGE_PATH.'/cache',
+        'auto_reload' => true,
+    ]);
+    $twig->addExtension(new IntlExtension());
+    $app->add(TwigMiddleware::create($app, $twig));
+} catch (LoaderError $e) {
+    var_dump($e);
 }
 
-$app = new App($container, $router, $request);
-
-$app->boot()->run();
+$app->run();
