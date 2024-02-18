f8ef4720ab8782d6604187a08840af4713c17f1c
GavinZ5160 <gavin.z.5160@gmail.com>
2024-02-18 23:03:26 +0800

    solve migration error

--- expennies/configs/migrations.php
+++ expennies/configs/migrations.php
@@ -6,7 +6,7 @@ return [
     'table_storage'           => [
         'table_name'                 => 'migrations',
         'version_column_name'        => 'version',
-        'version_column_length'      => 1024,
+        'version_column_length'      => 512,
         'executed_at_column_name'    => 'executed_at',
         'execution_time_column_name' => 'execution_time',
     ],
