888e26e319d28cadbae9a25ae7d8fd25c50ac67d
zuoyi615 <zuoyi615@gmail.com>
2024-02-29 16:38:17 +0800

    add Column mediaType

--- expennies/app/Entity/Receipt.php
+++ expennies/app/Entity/Receipt.php
@@ -22,8 +22,11 @@ class Receipt
     #[Column(name: 'file_name')]
     private string      $filename;
 
-    #[Column(name:'storage_filename')]
+    #[Column(name: 'storage_filename')]
     private string      $storageFilename;
+
+    #[Column(name: 'media_type')]
+    private string      $mediaType;
 
     #[Column(name: 'created_at')]
     private DateTime    $createdAt;
@@ -81,6 +84,18 @@ class Receipt
     public function setStorageFilename(string $storageFilename): Receipt
     {
         $this->storageFilename = $storageFilename;
+        return $this;
+    }
+
+    public function getMediaType(): string
+    {
+        return $this->mediaType;
+    }
+
+    public function setMediaType(string $mediaType): Receipt
+    {
+        $this->mediaType = $mediaType;
+
         return $this;
     }
 
