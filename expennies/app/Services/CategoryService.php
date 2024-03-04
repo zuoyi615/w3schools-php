19205982db80acbc2fa2d644ce0f2ed320bdc42e
zuoyi615 <zuoyi615@gmail.com>
2024-03-04 16:14:16 +0800

    init from P22

---
+++ expennies/app/Services/CategoryService.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\DataObjects\DataTableQueryParams;
+use App\Entity\Category;
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+
+readonly class CategoryService
+{
+
+    public function __construct(private EntityManager $em) {}
+
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
+    public function create(string $name, User $user): Category
+    {
+        $category = new Category();
+
+        $category->setUser($user);
+
+        $this->update($category, $name);
+
+        return $category;
+    }
+
+    public function getPaginatedCategories(DataTableQueryParams $params): Paginator
+    {
+        $query = $this
+            ->em
+            ->getRepository(Category::class)
+            ->createQueryBuilder('c')
+            ->setFirstResult($params->start)
+            ->setMaxResults($params->length);
+
+        $orderBy  = $params->orderBy;
+        $orderBy  = in_array($orderBy, ['name', 'createdAt', 'updatedAt']) ? $orderBy : 'createdAt';
+        $orderDir = strtolower($params->orderDir) === 'asc' ? 'asc' : 'desc';
+
+        $search = $params->search;
+        if (!empty($search)) {
+            $search = addcslashes($search, '%_');
+            $query->where('c.name LIKE :name')->setParameter('name', '%'.$search.'%');
+        }
+
+        $query->orderBy('c.'.$orderBy, $orderDir);
+
+        return new Paginator($query);
+    }
+
+    /**
+     * @throws ORMException
+     */
+    public function delete(int $id): void
+    {
+        $category = $this
+            ->em
+            ->getRepository(Category::class)
+            ->find($id);
+
+        if ($category) {
+            $this->em->remove($category);
+            $this->em->flush();
+        }
+    }
+
+    public function getById(int $id): ?Category
+    {
+        return $this
+            ->em
+            ->getRepository(Category::class)
+            ->find($id);
+    }
+
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
+    public function update(Category $category, string $name): Category
+    {
+        $category->setName($name);
+
+        $this->em->persist($category);
+        $this->em->flush();
+
+        return $category;
+    }
+
+    public function getCategoryNames(): array
+    {
+        return $this
+            ->em
+            ->getRepository(Category::class)
+            ->createQueryBuilder('c')
+            ->select('c.id', 'c.name')
+            ->getQuery()
+            ->getArrayResult();
+    }
+
+    public function findByName(string $name): ?Category
+    {
+        return $this->em->getRepository(Category::class)->findOneBy(['name' => $name]) ?? null;
+    }
+
+
+}
---
+++ expennies/configs/routes/web.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Controllers\AuthController;
+use App\Controllers\CategoryController;
+use App\Controllers\HomeController;
+use App\Controllers\ReceiptController;
+use App\Controllers\TransactionController;
+use App\Middleware\AuthMiddleware;
+use App\Middleware\GuestMiddleware;
+use Slim\App;
+use Slim\Routing\RouteCollectorProxy;
+
+return function (App $app) {
+    $app
+        ->get('/', [HomeController::class, 'index'])
+        ->add(AuthMiddleware::class);
+
+    $app
+        ->group('', function (RouteCollectorProxy $guest) {
+            $guest->get('/login', [AuthController::class, 'loginView']);
+            $guest->post('/login', [AuthController::class, 'login']);
+            $guest->get('/register', [AuthController::class, 'registerView']);
+            $guest->post('/register', [AuthController::class, 'register']);
+        })
+        ->add(GuestMiddleware::class);
+
+    $app
+        ->post('/logout', [AuthController::class, 'logout'])
+        ->add(AuthMiddleware::class);
+
+    $app
+        ->group('/categories', function (RouteCollectorProxy $categories) {
+            $categories->get('', [CategoryController::class, 'index']);
+            $categories->post('', [CategoryController::class, 'store']);
+            $categories->get('/load', [CategoryController::class, 'load']);
+
+            $id = '/{id:[0-9]+}';
+            $categories->delete($id, [CategoryController::class, 'delete']);
+            $categories->get($id, [CategoryController::class, 'get']);
+            $categories->post($id, [CategoryController::class, 'update']);
+        })
+        ->add(AuthMiddleware::class);
+
+    $app
+        ->group('/transactions', function (RouteCollectorProxy $transactions) {
+            $transactions->get('', [TransactionController::class, 'index']);
+            $transactions->post('', [TransactionController::class, 'store']);
+            $transactions->get('/load', [TransactionController::class, 'load']);
+            $transactions->post('/import', [TransactionController::class, 'import']);
+
+            $id = '/{id:[0-9]+}';
+            $transactions->delete($id, [TransactionController::class, 'delete']);
+            $transactions->get($id, [TransactionController::class, 'get']);
+            $transactions->post($id, [TransactionController::class, 'update']);
+            $transactions->post($id.'/receipts', [ReceiptController::class, 'store']);
+
+            $transactionId = '/{transactionId:[0-9]+}';
+            $transactions->get($transactionId.'/receipts'.$id, [ReceiptController::class, 'download']);
+            $transactions->delete($transactionId.'/receipts'.$id, [ReceiptController::class, 'delete']);
+        })
+        ->add(AuthMiddleware::class);
+};
