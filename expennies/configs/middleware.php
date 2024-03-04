7342d5812b38b7671de5d9a48b7697f0298078e2
zuoyi615 <zuoyi615@gmail.com>
2024-03-04 17:16:48 +0800

    add ClockworkMiddleware

--- expennies/configs/middleware.php
+++ expennies/configs/middleware.php
@@ -3,11 +3,14 @@
 declare(strict_types=1);
 
 use App\Config;
+use App\Enum\AppEnvironment;
 use App\Middleware\CsrfFieldsMiddleware;
 use App\Middleware\OldFormDataMiddleware;
 use App\Middleware\StartSessionsMiddleware;
 use App\Middleware\ValidationErrorsMiddleware;
 use App\Middleware\ValidationExceptionMiddleware;
+use Clockwork\Clockwork;
+use Clockwork\Support\Slim\ClockworkMiddleware;
 use Slim\App;
 use Slim\Middleware\MethodOverrideMiddleware;
 use Slim\Views\Twig;
@@ -15,6 +18,7 @@ use Slim\Views\TwigMiddleware;
 
 return function (App $app) {
     $container = $app->getContainer();
+    /** @var \App\Config $config */
     $config = $container->get(Config::class);
 
     $app->add(MethodOverrideMiddleware::class);
@@ -25,10 +29,15 @@ return function (App $app) {
     $app->add(ValidationErrorsMiddleware::class);
     $app->add(OldFormDataMiddleware::class);
     $app->add(StartSessionsMiddleware::class);
+
+    if (AppEnvironment::isDevelopment($config->get('app_environment'))) {
+        $app->add(new ClockworkMiddleware($app, $container->get(Clockwork::class)));
+    }
+
     $app->addBodyParsingMiddleware();
     $app->addErrorMiddleware(
-        (bool)$config->get('display_error_details'),
-        (bool)$config->get('log_errors'),
-        (bool)$config->get('log_error_details')
+        (bool) $config->get('display_error_details'),
+        (bool) $config->get('log_errors'),
+        (bool) $config->get('log_error_details')
     );
 };
