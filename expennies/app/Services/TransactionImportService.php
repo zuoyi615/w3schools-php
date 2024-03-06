6d97499bb2deb64bc1f6cf9f33ca9dfa2e60a04d
GavinZ5160 <gavin.z.5160@gmail.com>
2024-03-07 05:24:30 +0800

    add Clockwork log

--- expennies/app/Services/TransactionImportService.php
+++ expennies/app/Services/TransactionImportService.php
@@ -4,6 +4,8 @@ namespace App\Services;
 
 use App\DataObjects\TransactionData;
 use App\Entity\User;
+use Clockwork\Clockwork;
+use Clockwork\Request\LogLevel;
 use DateTime;
 use Doctrine\ORM\EntityManager;
 
@@ -12,6 +14,7 @@ readonly class TransactionImportService
 
     public function __construct(
         private CategoryService    $categoryService,
+        private Clockwork          $clockwork,
         private EntityManager      $em,
         private TransactionService $transactionService,
     ) {}
@@ -30,6 +33,9 @@ readonly class TransactionImportService
         $categories = $this->categoryService->getAllKeyedByName();
 
         fgetcsv($resource); // first line is fields columns
+
+        $this->clockwork->log(LogLevel::DEBUG, 'Memory Usage Before: '.memory_get_usage());
+        $this->clockwork->log(LogLevel::DEBUG, 'Unit Of Work Before: '.$this->em->getUnitOfWork()->size());
 
         $count     = 0;
         $batchSize = 250;
@@ -60,6 +66,9 @@ readonly class TransactionImportService
         if ($count > 1) {
             $this->em->flush();
         }
+
+        $this->clockwork->log(LogLevel::DEBUG, 'Memory Usage After: '.memory_get_usage());
+        $this->clockwork->log(LogLevel::DEBUG, 'Unit Of Work After: '.$this->em->getUnitOfWork()->size());
     }
 
 }
