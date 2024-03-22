30b25b64874c9f95b707dc903ceb78353edfdce7
zuoyi615 <zuoyi615@gmail.com>
2024-03-22 14:52:27 +0800

    remove test redis code

--- expennies/app/Controllers/HomeController.php
+++ expennies/app/Controllers/HomeController.php
@@ -5,13 +5,12 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use Psr\Http\Message\ResponseInterface as Response;
-use Psr\SimpleCache\CacheInterface;
 use Slim\Views\Twig;
 
 readonly class HomeController
 {
 
-    public function __construct(private Twig $twig, private CacheInterface $cache) {}
+    public function __construct(private Twig $twig) {}
 
     /**
      * @throws \Twig\Error\SyntaxError
@@ -21,12 +20,6 @@ readonly class HomeController
      */
     public function index(Response $response): Response
     {
-        $this->cache->set('name', 'Jon', 10);
-        $this->cache->set('age', 17, 10);
-        $this->cache->setMultiple(['height' => 180, 'weight' => 65.8]);
-
-        var_dump($this->cache->getMultiple(['name', 'age', 'height', 'weight']));
-
         return $this->twig->render($response, 'dashboard.twig');
     }
 
