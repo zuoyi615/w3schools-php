7bafdf7ac05933b666b3463269fd12a54b9cb5b3
zuoyi615 <zuoyi615@gmail.com>
2024-10-21 17:19:59 +0800

    add code format and tips text

--- 01-basic/06-numbers.php
+++ 01-basic/06-numbers.php
@@ -6,49 +6,60 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+      code {
+        background: gainsboro;
+      }
+    </style>
   </head>
   <body>
     <ol>
       <li>
         <?php
+          echo '<code>is_int(5985)</code>';
           var_dump(is_int(5985));
-          echo '<br />';
+          echo '<code>is_int(59.85)</code>';
           var_dump(is_int(59.85));
         ?>
       </li>
       <li>
         <?php
+          echo '<code>is_float(59.85)</code>';
           var_dump(is_float(59.85));
         ?>
       </li>
       <li>
         <?php
+          echo '<code>is_infinite(1.9e411)</code>';
           var_dump(is_infinite(1.9e411));
-          echo '<br />';
+          echo '<code>is_finite(1.9e411)</code>';
           var_dump(is_finite(1.9e411));
         ?>
       </li>
       <li>
         <?php
+          echo '<code>is_nan(acos(8))</code>';
           var_dump(is_nan(acos(8)));
+          echo '<code>is_nan(acos(0.5))</code>';
+          var_dump(is_nan(acos(0.5)));
         ?>
       </li>
       <li>
         <?php
+          echo '<code>is_numeric(5985)</code>';
           var_dump(is_numeric(5985));
-          echo '<br />';
+          echo '<code>is_numeric(59.85)</code>';
           var_dump(is_numeric('59.85'));
-          echo '<br />';
+          echo '<code>is_numeric(\'59.85\'+100)</code>';
           var_dump(is_numeric('59.85' + 100));
-          echo '<br />';
+          echo '<code>is_numeric(\'Hello\')</code>';
           var_dump(is_numeric('Hello'));
-          echo '<br />';
+          echo '<code>is_numeric(0xff0000)</code>';
           var_dump(is_numeric(0xff0000));
-          echo '<br />';
+          echo '<code>is_numeric(\'0xff0000\')</code>';
           var_dump(is_numeric('0xff0000'));
-          echo '<br />';
+          echo '<code>is_numeric(0701)</code>';
           var_dump(is_numeric(0701));
-          echo '<br />';
         ?>
       </li>
       <li>
@@ -66,12 +77,13 @@
           $x = 'a2345.768';
           $int_cast = (int)$x;
           echo $int_cast; // this is 0, strange
-          echo '<br />';
+          var_dump(is_numeric($x));
           var_dump(is_nan($int_cast));
-          echo '<br />';
 
           $y = 0;
           echo (bool)$y;
+          echo '<code>(bool)$y</code>';
+          var_dump((bool)$y);
           if (!is_nan($int_cast)) echo 'BAD';
         ?>
       </li>
--- 01-basic/07-math.php
+++ 01-basic/07-math.php
@@ -6,48 +6,52 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>math</title>
+    <style>
+    code {
+      background: gainsboro;
+    }
+    </style>
   </head>
   <body>
     <ul>
       <li>
         <?php
-          echo pi();
+          echo '<code>pi()</code> : ', pi();
         ?>
       </li>
       <li>
         <?php
-          echo(min(0, 150, 30, 20, -8, -200));
+          echo '<code>min(0, 150, 30, 20, -8, -200)</code> : ', min(0, 150, 30, 20, -8, -200);
           echo('<br/>');
-          echo(max(0, 150, 30, 20, -8, -200));
+          echo '<code>max(0, 150, 30, 20, -8, -200)</code> : ', max(0, 150, 30, 20, -8, -200);
         ?>
       </li>
       <li>
         <?php
-          echo abs(-1);
+          echo '<code>abs(-1)</code> : ', abs(-1);
         ?>
       </li>
       <li>
         <?php
-          echo sqrt(81);
+          echo '<code>sqrt(81)</code> : ', sqrt(81);
         ?>
       </li>
       <li>
         <?php
-          echo round(0.60);
+          echo '<code>round(0.60)</code> : ', round(0.60);
           echo('<br/>');
-          echo round(0.49);
+          echo '<code>round(0.49)</code> : ', round(0.49);
         ?>
       </li>
       <li>
         <?php
-          echo rand();
+          echo '<code>rand()</code> : ', rand();
           echo('<br/>');
-          echo rand(10, 100);
+          echo '<code>rand(10, 100)</code> : ', rand(10, 100);
           echo('<br/>');
-          echo rand(0, 100000) / 100000;
+          echo '<code>rand(0, 100000) / 100000</code> : ', rand(0, 100000) / 100000;
           echo('<br/>');
-          echo rand(0, 1);
-          echo('<br/>');
+          echo '<code>rand(0, 1)</code> : ', rand(0, 1);
         ?>
       </li>
     </ul>
