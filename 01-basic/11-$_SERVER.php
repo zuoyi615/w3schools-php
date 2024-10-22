aea739ab3b8d0f73d8cdaf6e40f42e0d29c8fe87
zuoyi615 <zuoyi615@gmail.com>
2024-10-22 10:16:15 +0800

    $_SERVER

--- 01-basic/11-$_SERVER.php
+++ 01-basic/11-$_SERVER.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1) ?>
+<?php
+
+  declare(strict_types=1) ?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -7,33 +9,34 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+    code {
+      background: gainsboro;
+    }
+    </style>
   </head>
   <body>
-    <ul>
-      <li>
-        <?php
-          echo $_SERVER['PHP_SELF'];
-          echo '<br />';
-          echo $_SERVER['SERVER_NAME'];
-          echo '<br />';
-          echo $_SERVER['HTTP_HOST'];
-          echo '<br />';
-          if (isset($_SERVER['HTTP_REFERER'])) {
-            echo $_SERVER['HTTP_REFERER'];
-            echo '<br />';
-          }
-          echo $_SERVER['HTTP_USER_AGENT'];
-          echo '<br />';
-          echo $_SERVER['SCRIPT_NAME'];
-          echo '<br />';
-          echo $_SERVER['SERVER_PORT'];
-          echo '<br />';
-          echo $_SERVER['SERVER_PROTOCOL'];
-          echo '<br />';
-          echo $_SERVER['REQUEST_METHOD'];
-          echo '<br />';
-        ?>
-      </li>
-    </ul>
+    <?php
+      echo '<code>$_SERVER[\'PHP_SELF\']</code> : ', $_SERVER['PHP_SELF'];
+      echo '<br />';
+      echo '<code>$_SERVER[\'SERVER_NAME\']</code> : ', $_SERVER['SERVER_NAME'];
+      echo '<br />';
+      echo '<code>$_SERVER[\'HTTP_HOST\']</code> : ', $_SERVER['HTTP_HOST'];
+      echo '<br />';
+      if (isset($_SERVER['HTTP_REFERER'])) {
+        echo '<code>$_SERVER[\'HTTP_REFERER\']</code> : ', $_SERVER['HTTP_REFERER'];
+        echo '<br />';
+      }
+      echo '<code>$_SERVER[\'HTTP_USER_AGENT\']</code> : ', $_SERVER['HTTP_USER_AGENT'];
+      echo '<br />';
+      echo '<code>$_SERVER[\'SCRIPT_NAME\']</code> : ', $_SERVER['SCRIPT_NAME'];
+      echo '<br />';
+      echo '<code>$_SERVER[\'SERVER_PORT\']</code> : ', $_SERVER['SERVER_PORT'];
+      echo '<br />';
+      echo '<code>$_SERVER[\'/SERVER_PROTOCOL\']</code> : ', $_SERVER['SERVER_PROTOCOL'];
+      echo '<br />';
+      echo '<code>$_SERVER[\'REQUEST_METHOD\']</code> : ', $_SERVER['REQUEST_METHOD'];
+      echo '<br />';
+    ?>
   </body>
 </html>
