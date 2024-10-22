c39e63e2a2fd8f8dd8134e2a939d7e467ffeda3b
zuoyi615 <zuoyi615@gmail.com>
2024-10-22 10:46:11 +0800

    Predefined super globals

---
+++ 01-basic/15-Superglobals.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1); ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+  </head>
+  <body>
+
+    <?php
+      /**
+       * Predefined super globals in PHP
+       * - $GLOBALS
+       * - $_SERVER
+       * - $_REQUEST
+       * - $_POST
+       * - $_GET
+       * - $_ENV
+       * - $_COOKIE
+       * - $_SESSION
+       */
+    ?>
+  </body>
+</html>
