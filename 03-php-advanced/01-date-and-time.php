d78b3b3294717b50160a48e0533d4c57ccfb34db
zuoyi615 <zuoyi615@gmail.com>
2024-10-23 09:14:20 +0800

    Summary for Datetime

--- 03-php-advanced/01-date-and-time.php
+++ 03-php-advanced/01-date-and-time.php
@@ -1,5 +1,8 @@
-<?php declare(strict_types=1) ?>
-<?php date_default_timezone_set("Asia/Shanghai") ?>
+<?php
+
+  declare(strict_types=1) ?>
+    <?php
+  date_default_timezone_set("Asia/Shanghai") ?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -10,18 +13,31 @@
     <title>date and time</title>
   </head>
   <body>
+    <?php
+
+      /**
+       * #### Datetime:
+       * - date()
+       * - time()
+       * - mktime()
+       * - strtotime()
+       */
+
+    ?>
     <h2>date(format,timestamp)</h2>
     <ol>
       <li>
         <?php
-          echo "Today is " . date("Y/m/d") . "<br>";
-          echo "Today is " . date("Y.m.d") . "<br>";
-          echo "Today is " . date("Y-m-d") . "<br>";
-          echo "Today is " . date("l");
+          echo "Today is ".date("Y/m/d")."<br>";
+          echo "Today is ".date("Y.m.d")."<br>";
+          echo "Today is ".date("Y-m-d")."<br>";
+          echo "Today is ".date("l");
         ?>
       </li>
-      <li>&copy; 2010-<?php echo date("Y"); ?></li>
-      <li><?php echo "The time is " . date("H:i:sa"); ?></li>
+      <li>&copy; 2010-<?php
+          echo date("Y"); ?></li>
+      <li><?php
+          echo "The time is ".date("H:i:sa"); ?></li>
     </ol>
 
     <h2>mktime(hour, minute, second, month, day, year)</h2>
@@ -29,7 +45,7 @@
       <li>
         <?php
           $d = mktime(11, 14, 54, 8, 12, 2014);
-          echo "Created date is " . date("Y-m-d h:i:sa", $d);
+          echo "Created date is ".date("Y-m-d h:i:sa", $d);
         ?>
       </li>
     </ol>
@@ -39,28 +55,28 @@
       <li>
         <?php
           $d = strtotime("10:30pm April 15 2014");
-          echo "Created date is " . date("Y-m-d h:i:sa", $d);
+          echo "Created date is ".date("Y-m-d h:i:sa", $d);
         ?>
       </li>
       <li>
         <?php
           $d = strtotime("tomorrow");
-          echo date("Y-m-d h:i:sa", $d) . "<br>";
+          echo 'Tomorrow : ', date("Y-m-d h:i:sa", $d)."<br>";
 
           $d = strtotime("next Saturday");
-          echo date("Y-m-d h:i:sa", $d) . "<br>";
+          echo 'Next Saturday : ', date("Y-m-d h:i:sa", $d)."<br>";
 
           $d = strtotime("+3 Months");
-          echo date("Y-m-d h:i:sa", $d) . "<br>";
+          echo '+3 Months : ', date("Y-m-d h:i:sa", $d)."<br>";
         ?>
       </li>
       <li>
         <?php
           $start = strtotime('Saturday');
-          $end = strtotime('+6 weeks', $start);
+          $end   = strtotime('+6 weeks', $start);
 
           while ($start < $end) {
-            echo date('M d', $start), '<br/>';
+            echo date('Y-m-d', $start), '<br/>';
             $start = strtotime('+1 week', $start);
           }
         ?>
@@ -68,9 +84,9 @@
       <li>
         <?php
           $day = 60 * 60 * 24 * 1;
-          $d1 = strtotime('July 04');
-          $d2 = ceil((time() - $d1) / $day);
-          echo "There are " . $d2 . " days after 4th of July<br/>";
+          $d1  = strtotime('July 04');
+          $d2  = ceil((time() - $d1) / $day);
+          echo "There are ".$d2." days after 4th of July<br/>";
         ?>
       </li>
     </ol>
