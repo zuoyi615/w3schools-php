8b86c62083f846abb659a3aa777a9543811f4295
zuoyi615 <zuoyi615@gmail.com>
2024-10-25 16:55:06 +0800

    unregister_tick_function()

--- learn-php-the-right-way/1.19-declare.php
+++ learn-php-the-right-way/1.19-declare.php
@@ -1,27 +1,38 @@
 <?php
-  /**
-   * ## declare(directive)
-   *  - ticks
-   *  - encoding
-   *  - strict_types which is recommended
-   * @link https://www.php.net/manual/en/control-structures.declare.php
-   */
-
-  declare(strict_types=1);
-
-  function onTick(): void {
-    echo 'Tick' . '<br/>';
-  }
-
-  register_tick_function('onTick');
-
-  declare(ticks=3);
-  $i = 0;
-  $len = 10;
-  while ($i < $len) echo $i++ . '<br/>';
-
-  function test(): void { }
-
-  $result = test();
-  var_dump($result);     // NULL
-  echo gettype($result); // NULL
+    /**
+     * ## declare(directive)
+     *  - [Doc](https://www.php.net/manual/en/control-structures.declare.php)
+     *  - ticks
+     *  - encoding
+     *  - strict_types which is recommended
+     *
+     *
+     *
+     *  - gettype()
+     *  - register_tick_function()
+     *  - unregister_tick_function()
+     */
+    
+    declare(strict_types=1);
+    
+    function onTick(): void
+    {
+        echo 'Tick'.'<br/>';
+    }
+    
+    register_tick_function('onTick');
+    
+    declare(ticks=3);
+    $i   = 0;
+    $len = 10;
+    while ($i < $len) {
+        echo $i++.'<br/>';
+    }
+    
+    unregister_tick_function('onTick');
+    
+    function test(): void {}
+    
+    $result = test();
+    var_dump($result);     // NULL
+    echo gettype($result); // NULL
