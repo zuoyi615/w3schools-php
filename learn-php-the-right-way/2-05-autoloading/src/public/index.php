050beb391dd8d3eca3bd686f8289e78a3466ceaf
zuoyi615 <zuoyi615@gmail.com>
2024-11-03 23:20:18 +0800

    add comment

--- learn-php-the-right-way/2-05-autoloading/src/public/index.php
+++ learn-php-the-right-way/2-05-autoloading/src/public/index.php
@@ -1,16 +1,21 @@
 <?php
-
+    
     declare(strict_types=1);
-
+    
     // require_once '../app/PaymentGateway/Paddle/Transaction.php';
     // require_once '../app/PaymentGateway/Paddle/CustomerProfile.php';
     // require_once '../app/PaymentGateway/Stripe/Transaction.php';
     // require_once '../app/Notification/Email.php';
-
-
+    
     /**
+     * - spl_autoload_register()
+     * - str_replace()
+     * - lcfirst()
+     * - file_exists()
+     *
      * @link https://www.php-fig.org/
      */
+    
     spl_autoload_register(function ($class) {
         $class    = str_replace('\\', '/', $class);
         $filename = "$class.php";
@@ -20,8 +25,8 @@
             require_once $path;
         }
     });
-
+    
     use App\PaymentGateway\Paddle\Transaction;
-
+    
     $paddleTransaction = new Transaction();
     var_dump($paddleTransaction);
