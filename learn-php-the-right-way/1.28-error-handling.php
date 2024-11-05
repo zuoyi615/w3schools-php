f7922e27eb319309c9fefb981ce7bc0f48858200
zuoyi615 <zuoyi615@gmail.com>
2024-11-05 17:06:43 +0800

    set_error_handler() trigger_error()

--- learn-php-the-right-way/1.28-error-handling.php
+++ learn-php-the-right-way/1.28-error-handling.php
@@ -1,30 +1,27 @@
 <?php
-  // error_reporting(E_ALL & ~E_WARNING); // not report WARNING
-  // trigger_error('Example_error', E_USER_ERROR);
-  // trigger_error('Example error', E_USER_WARNING);
-
-  function errorHandler(
-    int     $type,
-    string  $message,
-    ?string $file,
-    ?int    $line,
-  ): bool {
-    echo <<<TEXT
-      <div style="margin-bottom:32px;background: lightgray; padding: 24px;">
-        <h2 style="color: red; margin: 0;">Error: </h2>
-        <ul>
-          <li>type: $type </li>
-          <li>message: $message </li>
-          <li>file: $file </li>
-          <li>line: $line </li>
-        </ul>
-      </div>
-    TEXT;
-    // exit; // exit php script
-    return true; // check whether stop executing PHP internal error handler
-  }
-
-  set_error_handler('errorHandler'); // set custom error handler
-  trigger_error('Example error', E_USER_ERROR);
-
-  echo 'Hello World';
+    // error_reporting(E_ALL & ~E_WARNING); // not report WARNING
+    // trigger_error('Example_error', E_USER_ERROR);
+    // trigger_error('Example error', E_USER_WARNING);
+    
+    function errorHandler(int $type, string $message, ?string $file, ?int $line): bool
+    {
+        echo <<<TEXT
+          <div style="margin-bottom:32px;background: lightgray; padding: 24px;">
+            <h2 style="color: red; margin: 0;">Error: </h2>
+            <ul>
+              <li>type: $type </li>
+              <li>message: $message </li>
+              <li>file: $file </li>
+              <li>line: $line </li>
+            </ul>
+          </div>
+        TEXT;
+        
+        // exit; // exit php script
+        return true; // check whether stop executing PHP internal error handler, false->stop, true->not stop
+    }
+    
+    set_error_handler('errorHandler'); // set custom error handler
+    trigger_error('Example error', E_USER_ERROR);
+    
+    echo 'Hello World';
