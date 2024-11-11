7b9041e2b39179d6a245e1bfe8beab59afb952e4
zuoyi615 <zuoyi615@gmail.com>
2024-11-11 14:38:07 +0800

    PSR-12 format style

--- learn-php-the-right-way/2.07-static-singleton.php
+++ learn-php-the-right-way/2.07-static-singleton.php
@@ -1,22 +1,27 @@
 <?php
-
-  declare(strict_types=1);
-
-  namespace Singleton;
-
-  class DB {
-    private static ?DB $instance = null;
-
-    public static function getInstance(array $config): ?DB {
-      if (self::$instance === null) {
-        self::$instance = new DB($config);
-      }
-      return self::$instance;
+    
+    declare(strict_types=1);
+    
+    namespace Singleton;
+    
+    class DB
+    {
+        
+        private static ?DB $instance = null;
+        
+        public static function getInstance(array $config): ?DB
+        {
+            if (self::$instance === null) {
+                self::$instance = new DB($config);
+            }
+            
+            return self::$instance;
+        }
+        
+        private function __construct(public array $config) {}
+        
     }
-
-    private function __construct(public array $config) {}
-  }
-
-  $db = DB::getInstance([]);
-  $db1 = DB::getInstance([]);
-  var_dump($db===$db1);
+    
+    $db  = DB::getInstance([]);
+    $db1 = DB::getInstance([]);
+    var_dump($db === $db1);
