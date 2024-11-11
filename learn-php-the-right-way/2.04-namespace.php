8dee5574d514f98209fab3195bc7651cccbcd356
zuoyi615 <zuoyi615@gmail.com>
2024-11-11 14:04:49 +0800

    change filename

--- learn-php-the-right-way/2.04-namespace.php
+++ learn-php-the-right-way/2.04-namespace.php
@@ -1,9 +1,9 @@
 <?php declare(strict_types=1);
 
-  require_once './2.24-namespace-paddle0.php';
-  require_once './2.24-namespace-paddle1.php';
-  require_once './2.24-namespace-notification.php';
-  require_once './2.24-namespace-stripe0.php';
+  require_once './2.04-namespace-paddle0.php';
+  require_once './2.04-namespace-paddle1.php';
+  require_once './2.04-namespace-notification.php';
+  require_once './2.04-namespace-stripe0.php';
   // require_once './2.24-namespace2.php';
   // var_dump(new PaymentGateway\Paddle\Transaction());
   // echo '<br />';
