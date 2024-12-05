7e3d52d3dc7f56ca8c8f8c98abb5ff6d79b82900
zuoyi615 <zuoyi615@gmail.com>
2024-12-05 14:42:23 +0800

    robust the code

--- expennies/app/Filters/UserFilter.php
+++ expennies/app/Filters/UserFilter.php
@@ -8,14 +8,14 @@ use Doctrine\ORM\Query\Filter\SQLFilter;
 
 class UserFilter extends SQLFilter
 {
-
+    
     public function addFilterConstraint(ClassMetadata $targetEntity, string $targetTableAlias): string
     {
-        if (!$targetEntity->getReflectionClass()->implementsInterface(OwnableInterface::class)) {
+        if (!$targetEntity->getReflectionClass()?->implementsInterface(OwnableInterface::class)) {
             return '';
         }
-
+        
         return $targetTableAlias.'.user_id='.$this->getParameter('user_id');
     }
-
+    
 }
--- expennies/app/Middleware/AuthMiddleware.php
+++ expennies/app/Middleware/AuthMiddleware.php
@@ -14,41 +14,39 @@ use Slim\Views\Twig;
 
 readonly class AuthMiddleware implements MiddlewareInterface
 {
-
+    
     public function __construct(
         private Twig                          $twig,
         private ResponseFactoryInterface      $factory,
         private AuthInterface                 $auth,
         private EntityManagerServiceInterface $em,
     ) {}
-
+    
     public function process(
         ServerRequestInterface  $request,
         RequestHandlerInterface $handler
     ): ResponseInterface {
-        if ($user = $this->auth->getUser()) {
-            $this->twig->getEnvironment()->addGlobal('auth', [
-                'id'   => $user->getId(),
-                'name' => $user->getName(),
-            ]);
-            $this->twig->getEnvironment()->addGlobal(
-                'current_route',
-                RouteContext::fromRequest($request)->getRoute()->getName()
-            );
-
-            $this
-                ->em
-                ->enableUserAuthFilter($user->getId());
-
-            $request = $request->withAttribute('user', $user);
-
-            return $handler->handle($request);
+        $user = $this->auth->getUser();
+        if (!$user) {
+            $this->auth->logout();
+            
+            return $this->factory->createResponse(302)->withHeader('Location', '/login');
         }
-
-        return $this
-            ->factory
-            ->createResponse(302)
-            ->withHeader('Location', '/login');
+        
+        $this->twig->getEnvironment()->addGlobal('auth', [
+            'id'   => $user->getId(),
+            'name' => $user->getName(),
+        ]);
+        $this->twig->getEnvironment()->addGlobal(
+            'current_route',
+            RouteContext::fromRequest($request)->getRoute()->getName()
+        );
+        
+        $this->em->enableUserAuthFilter($user->getId());
+        
+        $request = $request->withAttribute('user', $user);
+        
+        return $handler->handle($request);
     }
-
+    
 }
